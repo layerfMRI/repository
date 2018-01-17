c7c50d114516c088065ba7e856b44f39d46b5ce2
layerfMRI <33968961+layerfMRI@users.noreply.github.com>
2018-01-17 12:24:10 -0500

    Emily included

--- drift_s.sh
+++ drift_s.sh
@@ -6,7 +6,7 @@ echo "fange an 160208"
 
 3dTstat -mean -prefix mean_Bild.nii $1 -overwrite
 echo "mean done"
-3dDetrend -prefix detrended.nii -polort 10  $1 -overwrite
+3dDetrend -prefix detrended.nii -polort 20  $1 -overwrite
 echo "detrend done"
 3dcalc -a mean_Bild.nii -b detrended.nii -expr 'a+b' -prefix  detrended_$1 -overwrite -datum short
 rm mean_Bild.nii
