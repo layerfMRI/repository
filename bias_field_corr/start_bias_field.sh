1e22af424beeac566d9ee1d76cb497be2f883b1d
Renzo <layerfMRI@gmail.com>
2019-02-05 17:59:27 +0100

    new paths in BICO

--- bias_field_corr/start_bias_field.sh
+++ bias_field_corr/start_bias_field.sh
@@ -6,8 +6,8 @@ echo "fange an"
 cp $1 uncorr.nii
 
 echo "hole SPM motion batch"
-cp /Users/huberl/NeuroDebian/repository/bias_field_corr/Bias_field_script_job.m ./Bias_field_script_job.m
-/Applications/MATLAB_R2016a.app/bin/matlab -nodesktop -nosplash -r "Bias_field_script_job"
+cp /Users/l.huber/repository/bias_field_corr/Bias_field_script_job.m ./Bias_field_script_job.m
+/Applications/MATLAB_R2018b.app/bin/matlab -nodesktop -nosplash -r "Bias_field_script_job"
 
 3dcalc -a muncorr.nii -prefix muncorr.nii -overwrite -expr 'a' -datum short
 
