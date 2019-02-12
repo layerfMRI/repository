97db4997a4096c69cc59b3172c3f47ab8862d69d
Renzo <layerfMRI@gmail.com>
2019-02-12 14:45:20 +0100

    align updated

--- Alignement_scripts/align_0.5mm_MP2RAGE_2_EPI/executed_command.sh
+++ Alignement_scripts/align_0.5mm_MP2RAGE_2_EPI/executed_command.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-#on felix run me with: sbatch --mem=100g --cpus-per-task=50 --time=14400:00  executed_command.sh
-
-fslcpgeom T1.nii mask.nii
-
-#antsApplyTransforms --interpolation BSpline[5] -d 3 -i mask.nii -o reg_mask.nii -r MP2RAGE.nii -t mask_matrix.txt
-
-
-module load ANTs
-
-ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=50
-export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS
+#This script can be downloaded from here: https://github.com/layerfMRI/repository/blob/master/Alignement_scripts/align_0.5mm_MP2RAGE_2_EPI/executed_command.sh 
 
 antsRegistration \
---verbose 1  \
+--verbose 1 \
 --dimensionality 3  \
 --float 0  \
 --collapse-output-transforms 1  \
@@ -23,20 +13,20 @@ antsRegistration \
 --winsorize-image-intensities [0.005,0.995]  \
 --initial-moving-transform initial_matrix.txt \
 --transform Rigid[0.1]  \
---metric MI[T1.nii,MP2RAGE.nii,1,32,Regular,0.25]  \
+--metric MI[EPI_T1.nii,ANAT.nii,1,32,Regular,0.25]  \
 --convergence [1000x500x250x100,1e-6,10]  \
 --shrink-factors 12x8x4x2  \
 --smoothing-sigmas 4x3x2x1vox  \
 -x mask.nii \
 --transform Affine[0.1]  \
---metric MI[T1.nii,MP2RAGE.nii,1,32,Regular,0.25]  \
+--metric MI[EPI_T1.nii,ANAT.nii,1,32,Regular,0.25]  \
 --convergence [1000x500x250x100,1e-6,10]  \
 --shrink-factors 12x8x4x2  \
 --smoothing-sigmas 4x3x2x1vox  \
 -x mask.nii \
 --transform SyN[0.1,3,0]  \
---metric CC[T1.nii,MP2RAGE.nii,1,4]  \
---convergence [100x100x70x50x20,1e-6,10]  \
+--metric CC[EPI_T1.nii,ANAT.nii,1,4]  \
+--convergence [50x50x70x50x20,1e-6,10]  \
 --shrink-factors 10x6x4x2x1  \
 --smoothing-sigmas 5x3x2x1x0vox  \
 -x mask.nii
