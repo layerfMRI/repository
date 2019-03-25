1d25c4a383218dd648e7c36e9b5638ed1572fc3b
Renzo <layerfMRI@gmail.com>
2019-03-25 14:22:08 +0100

    conv vov non NIH enhanced

---
+++ moco/conv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+echo "fange an"
+mkdir ./nii
+
+cd ./*
+
+
+isisconv -in . -out ../nii/S{sequenceNumber}_{sequenceDescription}_{echoTime}_{coilChannelMask}.nii -wdialect fsl -repn s16bit
+
+
+# in odert of make gtate work is needs to be installed on mac: "brew install coreutils" 
+
+
+cd ../
+
+
+#touch war_hier.txt
+
+#isisconv -in . -out ../nii/S{sequenceNumber}_{sequenceDescription}_{echoTime}_{coilChannelMask}.nii -wdialect fsl -repn s16bit
+
+echo "und tschuess"
+
+ 
