59bb59d206e88aa7b7434fc81c6ed97250867110
Renzo <layerfMRI@gmail.com>
2020-01-27 09:55:33 +0100

    moco added

--- ants_bico.sh
+++ ants_bico.sh
@@ -4,7 +4,7 @@
 echo "fange an"
 
 
-3dAutomask -prefix mask.nii -peels 3 -dilate 2 -overwrite $1
+3dAutomask -prefix mask.nii -peels 3 -dilate 2 $1
 
 N4BiasFieldCorrection -d 3 -i $1 -r 1 -s 1 -x mask.nii -o bico_$1
 
--- mnt_dabeast.sh
+++ mnt_dabeast.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 
-#sshpass -p 1 sshfs renzo@137.120.137.94:/mnt/ /mnt/ssh/var/www/dabeast/
-$echo 1 |  sshfs renzo@137.120.137.94:/mnt/ /mnt/ssh/var/www/dabeast/
+$echo 1 |  sshfs renzo@137.120.137.222:/mnt/ /mnt/ssh/var/www/
 
