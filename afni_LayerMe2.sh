7892fe66d9ccce4df1e6c8d88b7df6ef0b1567a1
Renzo <layerfMRI@gmail.com>
2020-12-06 15:57:31 +0100

    naming convention

---
+++ afni_LayerMe2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#get mean value
+3dROIstats -mask $2 -1DRformat -quiet -nzmean $1 > layer_t.dat
+#get standard deviation
+3dROIstats -mask $2 -1DRformat -quiet -sigma $1 >> layer_t.dat
+#get number of voxels in each layer
+3dROIstats -mask $2 -1DRformat -quiet -nzvoxels $1 >> layer_t.dat
+#format file to be in columns, so gnuplot can read it.
+WRD=$(head -n 1 layer_t.dat|wc -w); for((i=2;i<=$WRD;i=i+2)); do awk '{print $'$i'}' layer_t.dat| tr '\n' ' ';echo; done > layer.dat
+
+
+1dplot -sepscl layer.dat 
+
+
+mv layer.dat $2_Layer.dat
--- rename_VASO.sh
+++ rename_VASO.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mv Not_Nulled_Basis_0a.nii Not_Nulled_Basis_a.nii
+mv Not_Nulled_Basis_*a.nii Not_Nulled_Basis_a.nii
 
-mv Nulled_Basis_0b.nii Nulled_Basis_b.nii
+mv Nulled_Basis_*b.nii Nulled_Basis_b.nii
