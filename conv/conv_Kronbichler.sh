7079eae56378323348eaca12a5f1e0f90931a80b
Renzo <layerfMRI@gmail.com>
2021-01-29 15:48:55 +0100

    merginf og VE_ nii converter

--- conv/conv_Kronbichler.sh
+++ conv/conv_Kronbichler.sh
@@ -17,6 +17,23 @@ fslmerge -t nulled.nii    S1_1_1_1_*
 fslmerge -t notnulled.nii S1_1_1_2_*
 rm S1_1_1*.nii
 
+
+##################################################################################### 
+#### combining odd and even images that are stored in seperate files a la zipper ####
+#####################################################################################
+# 0 2 4 6 8 ...
+fslsplit nulled e_
+# 1 3 5 7 9 ...
+fslsplit notnulled o_
+
+la=`imglob -extension [eo]_*`
+lb=`ls -1 $la | sort -t _ -k 2`
+fslmerge -t combined.nii $lb
+
+rm e_*.nii
+rm 0_*.nii
+
+
 #MOCO_Kronbichler.sh 
 #BOCO.sh 
 
