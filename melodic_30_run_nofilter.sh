1985c69ad0c25c58db05f05b1f333a15d3b0afc3
Renzo <layerfMRI@gmail.com>
2021-02-26 12:22:09 +0100

    Cleaning

--- melodic_30_run_nofilter.sh
+++ melodic_30_run_nofilter.sh
@@ -2,4 +2,12 @@
 
 #This does the drift correction and a subsequence melodic into 30 ICs
 
+
+# FILTERING, if you want
+#3dTstat -mean -prefix mean_Bild_$1 $1 -overwrite
+#fslmaths $1 -bptf 20 4 filtered_$1
+#3dcalc -a mean_Bild_$1 -b filtered_$1 -expr 'a+b' -prefix  fsl_filtered_$1 -overwrite
+#rm filtered_$1
+#melodic -i $1 --nomask --nobet -d 30
+
 melodic -i $1 --nomask --nobet -d 30
--- melodic_50_run_nofilter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#This does the drift correction and a subsequence melodic into 50 ICs
-
-melodic -i $1 --nomask --nobet -d 50
