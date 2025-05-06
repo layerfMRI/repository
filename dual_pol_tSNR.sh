e6c8676df689c8183861cbf54e1234508cd241f6
layerfMRI <33968961+layerfMRI@users.noreply.github.com>
2025-05-06 09:17:04 -0400

    Update dual_pol_tSNR.sh

--- dual_pol_tSNR.sh
+++ dual_pol_tSNR.sh
@@ -2,32 +2,21 @@
 
 #generating motion mask
 
-NumVol=`3dinfo -nv S00_AEPIG_8_test.nii.gz`
+
+echo $1
+
+NumVol=`3dinfo -nv $1`
 echo $NumVol
+3dTcat -overwrite -prefix npo_$1 $1'[1]'    $1'[0..'`expr $NumVol - 2`']'
+3dTcat -overwrite -prefix nmo_$1 $1'[1..$]' $1'['`expr $NumVol - 2`']'
+3dCalc -a $1 -b npo_$1 -c nmo_$1 -overwrite -prefix calib_$1 -expr '0.5*a+0.25*b+0.25*c' 
+3dCalc -a $1 -b npo_$1 -c nmo_$1 -overwrite -prefix error_$1 -expr 'abs(0.5*a-0.25*b-0.25*c)' 
+3dTstat -mean -prefix meanerror_$1 error_$1'[2..'`expr $NumVol - 2`']'
 
-for filename in moco_S*_AEPIG*.nii.gz
-do
-echo $filename
-
-NumVol=`3dinfo -nv ${filename}`
-echo $NumVol
-3dTcat -overwrite -prefix npo_${filename} ${filename}'[1]'    ${filename}'[0..'`expr $NumVol - 2`']'
-3dTcat -overwrite -prefix nmo_${filename} ${filename}'[1..$]' ${filename}'['`expr $NumVol - 2`']'
-3dCalc -a ${filename} -b npo_${filename} -c nmo_${filename} -overwrite -prefix calib_${filename} -expr '0.5*a+0.25*b+0.25*c' 
-3dCalc -a ${filename} -b npo_${filename} -c nmo_${filename} -overwrite -prefix error_${filename} -expr 'abs(0.5*a-0.25*b-0.25*c)' 
-3dTstat -mean -prefix meanerror_${filename} error_${filename}'[2..'`expr $NumVol - 2`']'
-
-rm nmo_${filename}
-rm npo_${filename}
-
-done 
+rm nmo_$1
+rm npo_$1
 
 
-for filename in moco_S*_AEPIG*.nii.gz
-do
-echo $filename
-oddeven.sh ${filename}
 
-done 
 
 echo "done"
