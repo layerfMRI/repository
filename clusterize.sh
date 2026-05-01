88535a6e338c9f07911b2864c5dcb3ab1400f707
layerfMRI <33968961+layerfMRI@users.noreply.github.com>
2026-05-01 13:33:08 -0400

    Update clusterize.sh

--- clusterize.sh
+++ clusterize.sh
@@ -6,5 +6,6 @@
 
 
 3dclust -prefix nice_${filename} -1clip 1.6 0.8 10 ${filename} -overwrite
+3dclust -prefix noneg_${filename} -1noneg 1.6 0.8 10 ${filename} -overwrite
 
 #3dclust masked_file        -1clip val_thresh distance vol ${filename} -overwrite
