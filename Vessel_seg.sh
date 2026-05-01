dff1836d4963fc54fb7e9326a439ae28a0890aef
layerfMRI <33968961+layerfMRI@users.noreply.github.com>
2026-05-01 13:49:51 -0400

    Create Vessel_seg.sh

---
+++ Vessel_seg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+echo "fange an"
+
+#ResampleImage 3 $1 lowres_$1 0.7x0.7x0.7 0 3['l'] 6
+# Itksapnresample
+
+python /Users/administrator/Git/repository/Vessynth/vessynth_test.py -i ./lowres_3T2template_template0.nii.gz -o ./ -mod T2star\n
+
+
+
+echo "und tschuess"
+
+ 
